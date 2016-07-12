7f1befd03a43fa8ac8e98e1d014f19f7c12ca831
chengchao103 <chengchao103@163.com>
2016-07-12 17:48:31 +0800

    新增

---
+++ src/main/java/tk/mybatis/springboot/conf/Test.java
@@ -0,0 +1,12 @@
+package tk.mybatis.springboot.conf;
+
+/**   
+ * @ClassName:  Test   
+ * @Description:   
+ * @author: chengchao
+ * @date:   2016年7月12日 下午5:48:15   
+ * @Copyright(c) gome inc Gome Co.,LTD   
+ */
+public class Test {
+
+}
