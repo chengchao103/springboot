f877ac555c6d77ae29cc67047c96aef8295565a7
chengchao103 <chengchao103@163.com>
2016-07-12 17:25:56 +0800

    da

--- src/main/java/tk/mybatis/springboot/conf/MyBatisConfig.java
+++ src/main/java/tk/mybatis/springboot/conf/MyBatisConfig.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
+  
 package tk.mybatis.springboot.conf;
 
 import com.github.pagehelper.PageHelper;
