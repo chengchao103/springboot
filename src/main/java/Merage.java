7bca8a5e9557e3cef92aee6fa023598108f37cf7
chengchao103 <chengchao103@163.com>
2016-07-13 15:43:43 +0800

    das

---
+++ src/main/java/Merage.java
@@ -0,0 +1,11 @@
+
+/**   
+ * @ClassName:  Merage   
+ * @Description:   
+ * @author: chengchao
+ * @date:   2016年7月13日 下午3:43:31   
+ * @Copyright(c) gome inc Gome Co.,LTD   
+ */
+public class Merage {
+
+}
